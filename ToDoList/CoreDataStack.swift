c0edbc3840bf0873147370b0a7f4e404e55eee9e
Bobby Conti <noname@email.com>
2019-06-29 17:05:28 -0400

    Refactored CoreData stack using NSPersistentContainer

--- ToDoList/CoreDataStack.swift
+++ ToDoList/CoreDataStack.swift
@@ -10,38 +10,48 @@ import Foundation
 import CoreData
 
 class CoreDataStack {
-    private(set) lazy var applicationDocumentsDirectory: URL = {
-        let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        
-        let endIndex = urls.index(before: urls.endIndex)
-        return urls[endIndex]
-    }()
     
-    private(set) lazy var managedObjectModel: NSManagedObjectModel = {
-        let modelUrl = Bundle.main.url(forResource: "ToDoList", withExtension: "momd")!
-        return NSManagedObjectModel(contentsOf: modelUrl)!
-    }()
-    
-    private(set) lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
-        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
-        let url = self.applicationDocumentsDirectory.appendingPathComponent("ToDoList.sqlite")
-        
-        do {
-            try coordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: url)
-        } catch {
-            print(error)
-            abort()
-        }
-        
-        return coordinator
-    }()
+    // Refactored using NSPersistentContainer
+//    private(set) lazy var applicationDocumentsDirectory: URL = {
+//        let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+//
+//        let endIndex = urls.index(before: urls.endIndex)
+//        return urls[endIndex]
+//    }()
+//
+//    private(set) lazy var managedObjectModel: NSManagedObjectModel = {
+//        let modelUrl = Bundle.main.url(forResource: "ToDoList", withExtension: "momd")!
+//        return NSManagedObjectModel(contentsOf: modelUrl)!
+//    }()
+//
+//    private(set) lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+//        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+//        let url = self.applicationDocumentsDirectory.appendingPathComponent("ToDoList.sqlite")
+//
+//        do {
+//            try coordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: url)
+//        } catch {
+//            print(error)
+//            abort()
+//        }
+//
+//        return coordinator
+//    }()
     
     lazy var managedObjectContext: NSManagedObjectContext = {
-        let coordinator = self.persistentStoreCoordinator
-        let moc = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
-        moc.persistentStoreCoordinator = coordinator
+        let container = self.persistentContainer
+        return container.viewContext
+    }()
+    
+    private lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "ToDoList")
+        container.loadPersistentStores() { storeDescription, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error: \(error), \(error.userInfo)")
+            }
+        }
         
-        return moc
+        return container
     }()
 }
 
