77e4c08b163b9e2a2ee249cc5fe6e3c0b8fd83ce
Bobby Conti <noname@email.com>
2019-06-29 16:59:40 -0400

    Completed Core Data stack with managed object model, coordinator and context

--- ToDoList/CoreDataStack.swift
+++ ToDoList/CoreDataStack.swift
@@ -7,12 +7,41 @@
 //
 
 import Foundation
+import CoreData
 
 class CoreDataStack {
-    lazy var applicationDocumentsDirectory: URL = {
+    private(set) lazy var applicationDocumentsDirectory: URL = {
         let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         
         let endIndex = urls.index(before: urls.endIndex)
         return urls[endIndex]
     }()
+    
+    private(set) lazy var managedObjectModel: NSManagedObjectModel = {
+        let modelUrl = Bundle.main.url(forResource: "ToDoList", withExtension: "momd")!
+        return NSManagedObjectModel(contentsOf: modelUrl)!
+    }()
+    
+    private(set) lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+        let url = self.applicationDocumentsDirectory.appendingPathComponent("ToDoList.sqlite")
+        
+        do {
+            try coordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: url)
+        } catch {
+            print(error)
+            abort()
+        }
+        
+        return coordinator
+    }()
+    
+    lazy var managedObjectContext: NSManagedObjectContext = {
+        let coordinator = self.persistentStoreCoordinator
+        let moc = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        moc.persistentStoreCoordinator = coordinator
+        
+        return moc
+    }()
 }
+
