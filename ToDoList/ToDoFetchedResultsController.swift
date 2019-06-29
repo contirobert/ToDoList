9cc77b3a677a2a933100309563918a159f5a6327
Bobby Conti <noname@email.com>
2019-06-29 19:39:10 -0400

    Implemented animations for insert, delete, update and move operations

--- ToDoList/ToDoFetchedResultsController.swift
+++ ToDoList/ToDoFetchedResultsController.swift
@@ -32,7 +32,26 @@ class ToDoFetchedResultsController: NSFetchedResultsController<Item>, NSFetchedR
     
     // MARK: - Fetched Results Controller Delegate
     
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        
+        switch type {
+        case .insert:
+            guard let newIndexPath = newIndexPath else { return }
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        case .delete:
+            guard let indexPath = indexPath else { return }
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        case .update, .move:
+            guard let indexPath = indexPath else { return }
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
     func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        tableView.reloadData()
+        tableView.endUpdates()
     }
 }
